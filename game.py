bd412a38be56a503f0955a6a9f5f2a2eb6bbef54
Galaxy970 <zhiroto970@gmail.com>
2024-07-03 23:08:52 +0900

    HoT: initial revision

---
+++ game.py
@@ -0,0 +1,25 @@
+import random
+count = 1
+accept = 0
+Headsnumber = 0
+Tailsnumber = 0
+
+print('Tossing a coin...')
+
+for number in range(3):
+    print('Round '+str(count)+' :', end = ' ')
+    accept = random.randint(1,100)
+
+    if accept%2 == 0:
+        print('Heads')
+        Headsnumber += 1
+
+    else:
+        print('Tails')
+        Tailsnumber += 1
+
+    count += 1
+
+print('Heads:'+str(Headsnumber)+',Tails:'+str(Tailsnumber))
+
+
